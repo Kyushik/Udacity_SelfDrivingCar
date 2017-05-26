dcbc6c5ff77e646a34238955caed78576bf52656
Kyushik <kyushikmin@gmail.com>
2017-05-27 07:26:53 +0900

    Finished calculate rmse in tools.cpp and initialization in ukf.cpp.

--- Term2/Project2_Unscented_Kalman_Filter/src/tools.cpp
+++ Term2/Project2_Unscented_Kalman_Filter/src/tools.cpp
@@ -15,4 +15,42 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // Check the validity
+  // 1. The estimation vector size should not be zero
+  // 2. The estimation vector size should be equal with ground truth vector size
+
+  if (estimations.size() != ground_truth.size())
+  {
+    std::cout << "Vector size is NOT equal!!!" << std::endl;
+    return rmse;
+  }
+
+  if (estimations.size() == 0)
+  {
+    std::cout << "Estimation vector size is 0!!!" << std::endl;
+    return rmse;
+  }
+
+  // Accumulate Squared Residuals
+  for(int i = 0; i < estimations.size(); ++i)
+  {
+    VectorXd residual = estimations[i] - ground_truth[i];
+
+    // Coefficient-wise multiplication
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // Calculate Mean
+  rmse = rmse / estimations.size();
+
+  // Calculate the Squared Root
+  rmse = rmse.array().sqrt();
+  
+  // Return rmse
+  return rmse;
 }
--- Term2/Project2_Unscented_Kalman_Filter/src/ukf.cpp
+++ Term2/Project2_Unscented_Kalman_Filter/src/ukf.cpp
@@ -52,6 +52,24 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  ///* Weights of sigma points
+  VectorXd weights_;
+
+  ///* State dimension
+  n_x_ = x_.size();
+
+  ///* Augmented state dimension
+  n_aug_ = 7;
+
+  ///* Sigma point spreading parameter
+  lambda_ = 3 - n_aug_;
+
+  ///* the current NIS for radar
+  NIS_radar_ = 0;
+
+  ///* the current NIS for laser
+  NIS_laser_ = 0;
 }
 
 UKF::~UKF() {}
@@ -67,6 +85,58 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+
+  // Initialization
+
+  if (!is_initialized_)
+  {
+    // Initialize the state x_ with the first measurement
+    // Create the covariance matrix
+    cout << "Unscented Kalman Filter: " << endl;
+    x_ = VectorXd(n_x_);
+    x_ << 1, 1, 1, 1, 1;
+
+    P_ = MatrixXd(n_x_, n_x_);
+    P_ << 1, 0, 0, 0, 0,
+          0, 1, 0, 0, 0,
+          0, 0, 1, 0, 0,
+          0, 0, 0, 1, 0,
+          0, 0, 0, 0, 1;
+
+    float previous_timestamp_ = meas_package.timestamp_;
+
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR)
+    {
+      // Convert radar from polar to cartesian coordinates and initialize state
+
+      float rho = meas_package.raw_measurements_(0);
+      float phi = meas_package.raw_measurements_(1);
+      float rho_dot = meas_package.raw_measurements_(2);
+
+      float vx = rho_dot * cos(phi);
+      float vy = rho_dot * sin(phi);
+      float v_ = sqrt(vx * vx + vy * vy);
+
+      x_ << rho * cos(phi),
+            rho * sin(phi),
+            v_,
+            0,
+            0;
+     }
+     else if (meas_package.sensor_type_ == MeasurementPackage::LASER)
+     {
+       // Initialize state
+       x_ << meas_package.raw_measurements_(0),
+             meas_package.raw_measurements_(1),
+             0,
+             0,
+             0;
+     }
+
+    // Done initializing
+    is_initialized_ = true;
+    return;
+  }
 }
 
 /**
@@ -81,6 +151,12 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  MatrixXd X_sig_aug  = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+  MatrixXd X_sig_pred = MatrixXd(n_x_, 2 * n_aug_ + 1); 
+
+  MatrixXd A = P_.llt().matrixL();
+
+
 }
 
 /**
